6eeb742f12b7234e1c3ebae4923fb62223eed8d2
MarcoRamirezGT <maquito156@gmail.com>
2022-08-01 18:52:06 -0600

    Analisis exploratorio

--- Script1.R
+++ Script1.R
@@ -4,3 +4,17 @@ library("readxl")
 # xls files
 importacion <- read_excel("importacion.xlsx")
 View(importacion)
+str(importacion)
+importacion$Fecha<-as.Date(importacion$Fecha, "%Y/%m/%d")
+
+
+nrow(importacion)
+ncol(importacion)
+
+
+#Que mes fue el que mas tuvo mas importacion 
+superior <- importacion[order(importacion$`Gasolina superior`),]
+superior<- head(importacion,10)
+superior<- 
+View(superior)
+
